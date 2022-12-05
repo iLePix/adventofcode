1795e096ad458ad54088fc99785c6a5f2a6fde0d
Elias Hanelt <eliashanelt@student-net-etx-0890.intern.ethz.ch>
2022-12-05 10:03:46 +0100

    dayo5 clean up

--- 2022/day05/src/main.rs
+++ 2022/day05/src/main.rs
@@ -6,50 +6,60 @@ fn main() -> std::io::Result<()> {
     let reader = BufReader::new(file);
     let binding = reader.lines().filter_map(|lr| lr.ok()).collect::<Vec<String>>();
     let mut lines = binding.iter();
-    let mut stacks = parse_stacks(&mut lines);
-    /*for move_instruction in lines {
-        move_instruction.split(' ')
-        .enumerate()
-        .filter(|(i, _)| i % 2 == 1)
-        .map(|(_, i)| i.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>()
-        .chunks(3)
-        .for_each(|inst| {
-            let (amount, from, to) = (inst[0], inst[1] - 1, inst[2] - 1);
-            for _ in 0..amount {
-                let elem = stacks[from].pop().unwrap();
-                stacks[to].push(elem);
-            }
-        })
-    }*/
-    for move_instruction in lines {
-        move_instruction.split(' ')
-        .enumerate()
-        .filter(|(i, _)| i % 2 == 1)
-        .map(|(_, i)| i.parse::<usize>().unwrap())
-        .collect::<Vec<usize>>()
-        .chunks(3)
-        .for_each(|inst| {
-            let (amount, from, to) = (inst[0], inst[1] - 1, inst[2] - 1);
-            let elems = &stacks.clone()[from][(stacks[from].len() - amount)..stacks[from].len()];
-            stacks[to].append(&mut elems.to_vec());
-            for _ in 0..amount {
-                stacks[from].pop();
-            }
-        })
-    }
-
-
-
-    let first_elems = stacks.iter().filter_map(|stack| stack.last()).collect::<String>();
-    println!("{}", first_elems);
+    let stacks = parse_stacks(&mut lines);
+    let instructions = parse_instructions(&mut lines);
+    println!("Part 1: {}", part1(stacks.clone(), instructions.clone()));
+    println!("Part 2: {}", part2(stacks, instructions));
     Ok(())
 }
 
+fn part1(mut stacks: Vec<Vec<char>>, instructions: Vec<Instruction>) -> String {
+    for inst in instructions {
+        for _ in 0..inst.amount {
+            let elem = stacks[inst.from].pop().unwrap();
+            stacks[inst.to].push(elem);
+        }
+    }
+    stacks.iter().filter_map(|stack| stack.last()).collect::<String>()
+}
 
-fn parse_stacks(iter: &mut Iter<String>) -> Vec<Vec<char>> {
+fn part2(mut stacks: Vec<Vec<char>>, instructions: Vec<Instruction>) -> String {
+    for inst in instructions {
+            let elems = &stacks.clone()[inst.from][(stacks[inst.from].len() - inst.amount)..stacks[inst.from].len()];
+            stacks[inst.to].append(&mut elems.to_vec());
+            for _ in 0..inst.amount {
+                stacks[inst.from].pop();
+            }
+    }
+    stacks.iter().filter_map(|stack| stack.last()).collect::<String>()
+}
+
+#[derive(Clone, Copy, Debug)]
+struct Instruction {
+    amount: usize,
+    from: usize,
+    to: usize
+}
+
+
+fn parse_instructions(lines: &mut Iter<String>) -> Vec<Instruction> {
+    lines.filter(|l| !l.is_empty()).map(|inst| {
+        let elems = inst.split(' ')
+        .enumerate()
+        .filter(|(i, _)| i % 2 == 1)
+        .map(|(_, i)| i.parse::<usize>().unwrap())
+        .collect::<Vec<usize>>()
+        .chunks(3)
+        .map(|inst| Instruction {amount: inst[0], from: inst[1] - 1, to: inst[2] - 1})
+        .collect::<Vec<Instruction>>();
+        *elems.first().unwrap()
+    }).collect()
+}
+
+
+fn parse_stacks(lines: &mut Iter<String>) -> Vec<Vec<char>> {
     let mut stacks: Vec<Vec<char>> = vec![];
-    for (level, line) in iter.enumerate() {
+    for (level, line) in lines.enumerate() {
         if level == 0 {
             stacks = vec![vec![]; (line.len() / 4) + 1]
         }
@@ -69,7 +79,6 @@ fn parse_stacks(iter: &mut Iter<String>) -> Vec<Vec<char>> {
             }
         }
     }
-    iter.skip(1);
     for stack in &mut stacks {
         stack.reverse();
     }
