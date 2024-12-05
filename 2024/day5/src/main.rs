2079797fe154888f9fad8e4e5667ebbd8fef8a89
Elias Hanelt <elias.hanelt@gmail.com>
2024-12-05 06:59:32 +0100

    day5 part2

--- 2024/day5/src/main.rs
+++ 2024/day5/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{cmp::Ordering, collections::HashMap};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let input = std::fs::read_to_string("./input.txt")?;
@@ -13,7 +13,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .and_modify(|v| v.push(after))
             .or_insert(vec![after]);
     }
-    dbg!(&order_rules);
     let mut sum = 0;
     'upd_loop: for update in updates.split('\n') {
         let nums: Vec<i32> = update
@@ -23,17 +22,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         if nums.len() < 1 {
             break;
         }
-        for i in 1..nums.len() {
+        for i in 0..nums.len() {
             if order_rules
                 .get(&nums[i])
                 .is_some_and(|after| after.iter().any(|&item| nums[..i].contains(&item)))
             {
+                let mut new_order = nums.clone();
+                new_order.sort_by(|a, b| {
+                    if order_rules.get(&a).is_some_and(|after| after.contains(b)) {
+                        Ordering::Greater
+                    } else {
+                        Ordering::Less
+                    }
+                });
+                let middle = new_order[nums.len() / 2];
+                sum += middle;
                 continue 'upd_loop;
+                println!("found valid update {middle}");
             }
         }
-        let middle = nums[nums.len() / 2];
-        sum += middle;
-        println!("found valid update {middle}");
     }
     println!("{sum}");
     Ok(())
